2f25efbc12adf31ec11cee420a923194671300d4
shantheone <shandoor@gmail.com>
2026-04-10 15:24:59 +0200

    It seems we need at least one CWD_TOG key. Will investigate how to ignore it if both shift CAPS_WORD works

--- keyboards/adm42/v2/keymaps/shan/keymap.c
+++ keyboards/adm42/v2/keymaps/shan/keymap.c
@@ -186,7 +186,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_LWIN, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_RWIN,
             LC_CIRC, KC_LBRC, KC_RBRC, KC_LPRN, KC_RPRN, KC_EXLM, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_MINS, RC_DLR,
             KC_LALT, KC_AMPR, KC_AT,   KC_LCBR, KC_RCBR, KC_PIPE, KC_UNDS, KC_ASTR, KC_HASH, KC_PERC, KC_BSLS, LOR_ALT,
-                                       _______, _______, KC_DEL,  KC_ENT,  _______, _______,
+                                       XXXXXXX, _______, KC_DEL,  KC_ENT,  _______, CWD_TOG
+    ),
     [_EXTRA] = LAYOUT_3x12_6(
             LS_F11,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  RS_F12,
             KC_LCTL, KC_PAUS, KC_INS,  KC_VOLD, KC_VOLU, KC_MUTE, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_BTN1, KC_RCTL,
