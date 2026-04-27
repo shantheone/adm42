3fa1aaf1be03237a14774cca20b8e0c043225ecd
shantheone <shandoor@gmail.com>
2026-04-27 13:47:09 +0200

    Moved mouse keys

--- keyboards/adm42/v2/keymaps/shan/keymap.c
+++ keyboards/adm42/v2/keymaps/shan/keymap.c
@@ -226,9 +226,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     ),
     [_EXTRA] = LAYOUT_3x12_6(
             LW_F11,  KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  RW_F12,
-            KC_LCTL, KC_SLEP,    LSG(KC_4),  KC_VOLD,    KC_VOLU,    KC_MUTE, KC_HOME, KC_PGDN, KC_PGUP, KC_END,  KC_BTN1, KC_RCTL,
-            KC_LALT, LGUI(KC_Z), LGUI(KC_X), LGUI(KC_C), LGUI(KC_V), KC_MPLY, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_BTN2, LOR_ALT,
-                                       KC_ESC,  _______, KC_DEL,     _______, _______, KC_CAPS
+            KC_LCTL, KC_SLEP,    LSG(KC_4),  KC_VOLD,    KC_VOLU,    KC_MUTE, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, XXXXXXX, KC_RCTL,
+            KC_LALT, LGUI(KC_Z), LGUI(KC_X), LGUI(KC_C), LGUI(KC_V), KC_MPLY, KC_HOME, KC_PGDN, KC_MS_U, KC_END,  XXXXXXX, LOR_ALT,
+                                       KC_ESC,  _______, KC_DEL,     _______, KC_BTN1, KC_BTN2
     ),
     [_ADM] = LAYOUT_3x12_6(
             RGB_WPM, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, RGB_M_B, KC_COMM, KC_P7,  KC_P8, KC_P9, KC_PPLS, KC_PEQL,
