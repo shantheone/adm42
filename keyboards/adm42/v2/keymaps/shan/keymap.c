572533a6b73bc813d6a9c1edce2ae2f5b3515d8f
shantheone <shandoor@gmail.com>
2026-06-04 13:19:40 +0200

    Added a macro as a test (it's working)

--- keyboards/adm42/v2/keymaps/shan/keymap.c
+++ keyboards/adm42/v2/keymaps/shan/keymap.c
@@ -172,6 +172,9 @@ enum custom_keycodes {
     COMP_SEL,
     ALT_SEL,
     ERE_TOGG,
+    
+    // Macros
+    QMK_DATE,
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -197,7 +200,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_EXTRA] = LAYOUT_3x12_6(
             LS_F11,  KC_F1,      KC_F2,              KC_F3,              KC_F4,        KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,        RS_F12,
             KC_LCTL, KC_SLEP,    LSG(KC_4),          KC_VOLD,            KC_VOLU,      KC_MUTE, KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, KC_MS_WH_UP,   KC_RCTL,
-            KC_LALT, XXXXXXX,    KC_MISSION_CONTROL, LCTL(RSFT(KC_TAB)), LCTL(KC_TAB), KC_MPLY, KC_HOME, KC_PGDN, KC_MS_U, KC_END,  KC_MS_WH_DOWN, LOR_ALT,
+            KC_LALT, QMK_DATE,   KC_MISSION_CONTROL, LCTL(RSFT(KC_TAB)), LCTL(KC_TAB), KC_MPLY, KC_HOME, KC_PGDN, KC_MS_U, KC_END,  KC_MS_WH_DOWN, LOR_ALT,
                                        KC_ESC,  _______, KC_DEL,     _______, KC_BTN1, KC_BTN2
     ),
     [_ADM] = LAYOUT_3x12_6(
@@ -1015,6 +1018,8 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 
     if (record->event.pressed) {
         switch (keycode) {
+         case QMK_DATE:
+            SEND_STRING("19790814");
          case DF_QWER:
             set_single_persistent_default_layer(_QWERTY);
             return false;
