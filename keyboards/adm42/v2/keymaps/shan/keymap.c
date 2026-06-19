3677295ea0238690831eb32778b025e92eafbf14
shantheone <shandoor@gmail.com>
2026-06-19 12:39:02 +0200

    Added backslash and dash to alt keys

--- keyboards/adm42/v2/keymaps/shan/keymap.c
+++ keyboards/adm42/v2/keymaps/shan/keymap.c
@@ -179,9 +179,9 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT_3x12_6(
-            LS_GRV,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    RS_EQU,
-            LC_TAB,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, RC_QUT,
-            KC_LALT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RALT,
+            LS_GRV,          KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    RS_EQU,
+            LC_TAB,          KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, RC_QUT,
+            LALT_T(KC_BSLS), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, RALT_T(KC_UNDS),
                                        LLS_ESC, LW_SPC,  LLA_TAB, LLE_ENT, RW_BPC,  LLS_DEL
     ),
     [_SPECIAL] = LAYOUT_3x12_6(
